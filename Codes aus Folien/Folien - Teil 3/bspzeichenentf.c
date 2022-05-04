5e45ea1e2fdd532328f645e994763422e5f26050
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-05-04 17:35:13 +0200

    Create bspzeichenentf.c

---
+++ Codes aus Folien/Folien - Teil 3/bspzeichenentf.c
@@ -0,0 +1,17 @@
+// Folie 3 - bspzeichenentf.c
+ 
+#include <stdio.h>
+ 
+int main(void){
+    char s[] = "Mein Text\n";
+    char ch;
+    int i = 0;
+    
+    do {
+        ch = s[i]; // Zeichen holen
+        i++; // Index 1 weiter
+    } while ( ch != '\n' && ch != 0 );
+   
+    i--;
+    s[i] = 0; 
+ }
