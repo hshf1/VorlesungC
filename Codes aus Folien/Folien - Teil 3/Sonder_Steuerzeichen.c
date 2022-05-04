bbbad51ada2e3b663be4122501800cf9365e286e
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-05-04 15:58:05 +0200

    Create Sonder_Steuerzeichen.c

---
+++ Codes aus Folien/Folien - Teil 3/Sonder_Steuerzeichen.c
@@ -0,0 +1,11 @@
+// Folie 3 - Sonder_Steuerzeichen.c
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+   printf("Ueberlaenge mit Umlauten: \232berl\204nge\n\n");
+   printf("Andere Moeglichkeit:   %cberl%cnge\n\n",0x9A,0x84);
+   system("Pause");
+}
