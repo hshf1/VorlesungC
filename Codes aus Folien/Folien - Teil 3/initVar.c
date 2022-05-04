fa531b72157bf9aa01138f53e1132212e6ecdd59
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-05-04 15:07:44 +0200

    Update and rename BeispieleFuerKonstanten.c to initVar.c

---
+++ Codes aus Folien/Folien - Teil 3/initVar.c
@@ -0,0 +1,17 @@
+// Folie 3 - initVar.c
+ 
+#include <stdio.h>
+ 
+int main(void)
+{
+    int iBreite;
+    int iHoehe;
+    int iUmfang;
+    int iFlaeche;
+ 
+    iBreite = 5;
+    iHoehe = 5;
+    iUmfang = 2 * (iBreite + iHoehe);
+    iFlaeche = (iBreite * iHoehe);
+    return 0;
+}
