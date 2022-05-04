20f3da9e59c45209d0cb94cefe710571d3621f76
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-05-04 16:18:30 +0200

    Create iftest.c

---
+++ Codes aus Folien/Folien - Teil 3/iftest.c
@@ -0,0 +1,21 @@
+// Folie 3 - iftest.c
+ 
+#include <stdio.h>
+ 
+int main (void)
+{
+    int a = 1;
+    
+    // Bedingung 1
+    if (a+1>=2 && a<=2*a-1) printf("TRUE\n");
+    else printf("FALSE\n");
+    
+    // Bedingung 2
+    if (a+1<<1==4) printf("TRUE\n");
+    else printf("FALSE\n");
+    
+    // Bedingung 3
+    if (++a==2 && a*2==2) printf("TRUE\n");
+    else printf("FALSE\n");
+ 
+}
