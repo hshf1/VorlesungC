a3c72313161455b066209327a62b645b5b462df7
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-05-04 17:04:23 +0200

    Create bspintFeld.c

---
+++ Codes aus Folien/Folien - Teil 3/bspintFeld.c
@@ -0,0 +1,17 @@
+// Folie 3 - bspintFeld.c
+
+#include <stdio.h>
+
+int main(void){
+    int feld[] = {1,2,3,4,6,7,8,9,3,4};
+    int zaehler = 0;
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        if (feld[i] <= 4)
+        zaehler++;
+    }
+
+    printf("%d", zaehler);
+}
