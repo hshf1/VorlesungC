9cf1c7b1e6a31f3a7bd5a7d5eb44f3853fc4ff6f
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-05-04 16:21:45 +0200

    Create switchfallthrough.c

---
+++ Codes aus Folien/Folien - Teil 3/switchfallthrough.c
@@ -0,0 +1,22 @@
+// Folie 3 - switchfallthrough.c
+
+#include <stdio.h>
+
+int main (void)
+{
+    int iNote;
+
+    scanf("%d", &iNote);
+
+    switch (iNote)
+    {
+        case 1: printf("sehr gut");                         break;
+        case 2: printf("gut");                              break;
+        case 3:
+        case 4: printf("befriedigend oder ausreichend");    break;
+        case 5:
+        case 6: printf("mangelhaft oder ungenügend");       break;
+        default:
+        printf("Noten 1 - 6 bitte!");
+    }
+}
