64515f6336fe568baaddbd39ace2d83e9bdd5326
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-05-04 15:24:06 +0200

    Create bspausrichten.c

---
+++ Codes aus Folien/Folien - Teil 3/bspausrichten.c
@@ -0,0 +1,10 @@
+// Folie 3 - bspausrichten.c
+ 
+#include <stdio.h>
+ 
+int main(void)
+{
+    printf("Int rechts ausgerichtet:#%8d#\n", 100);
+    printf("Int links  ausgerichtet:#%-8d#\n", 100);
+    return 0;
+}
