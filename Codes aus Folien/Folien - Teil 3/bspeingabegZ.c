f55248fdd36ca500bcf6a9c042fb08baec50d95f
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-05-04 17:18:15 +0200

    Create bspeingabegZ.c

---
+++ Codes aus Folien/Folien - Teil 3/bspeingabegZ.c
@@ -0,0 +1,15 @@
+// Folie 3 - bspeingabegZ.c
+ 
+#include <stdio.h>
+
+int main(void){
+    char zeichen;
+  
+    printf("Geben Sie eine Ziffer ein:\n");
+ 
+    do {
+    zeichen = getchar();
+    // alternativ:
+    // scanf("%c", &zeichen);
+    } while (zeichen < '0'  || zeichen > '9');
+}
