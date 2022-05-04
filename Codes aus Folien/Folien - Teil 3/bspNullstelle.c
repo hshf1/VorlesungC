a048e266ef538003822ea37b665c54ca35cd0fd4
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-05-04 17:21:17 +0200

    Create bspNullstelle.c

---
+++ Codes aus Folien/Folien - Teil 3/bspNullstelle.c
@@ -0,0 +1,18 @@
+// Folie 3 - bspNullstelle.c
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    float x = 2; // Startwert
+    float y; // Funktionswert y an der Stelle x
+    float y_; // Ableitung an der Stelle x
+    
+    do {
+        y = pow(x,2) - 7;
+        y_ = 2*x;
+        x = x - y/y_;
+    } while (y > 0.01 || y < -0.01);
+    
+    printf("Nullstelle: %.4f", x);
+}
