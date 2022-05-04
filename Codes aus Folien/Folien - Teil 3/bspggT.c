8b2a5db30e3fef7fa78ea85b007f13ed885075e5
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-05-04 17:05:35 +0200

    Create bspggT.c

---
+++ Codes aus Folien/Folien - Teil 3/bspggT.c
@@ -0,0 +1,19 @@
+// Folie 3 - bspggT.c
+
+#include <stdio.h>
+
+int main(void){
+    int a = 56; 
+    int b = 49;
+
+    while (a != b)
+    {
+    // Re-Initialsierung von a oder b
+    if (a >= b)
+        a = a - b;
+    else
+        b = b - a; 
+    }
+    
+    printf("Ergebnis: %d", a);
+}
