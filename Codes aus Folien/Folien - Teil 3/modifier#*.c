6028b3427ad1ecb1756b31cf2962f44ad51b3b13
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-05-04 15:54:00 +0200

    Create modifier#*.c

---
+++ Codes aus Folien/Folien - Teil 3/modifier#*.c
@@ -0,0 +1,13 @@
+// Folie 3 - modifier#*.c
+ 
+#include <stdio.h>
+ 
+int main(void)  
+{
+    printf("Dezimalpunkt: |%#f|\n", 100);
+    printf("Dezimalpunkt: |%.0f|\n", 3.77);
+    printf("Dezimalpunkt: |%#.0f|\n", 3.77);
+    printf("Hexadezimalwert ausgeben: |%#x|\n",  0x65);
+    printf("Minimalwert und Genauigkeit: |%*.*f|\n", 15, 10, 123.456789);
+    return 0;
+}
