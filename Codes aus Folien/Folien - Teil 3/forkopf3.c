5835019ea95e72b3b665b18fd82c99c1b96f16f3
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-05-04 16:36:29 +0200

    Create forkopf3.c

---
+++ Codes aus Folien/Folien - Teil 3/forkopf3.c
@@ -0,0 +1,13 @@
+// Folie 3 - forkopf3.c
+ 
+#include <stdio.h>
+ 
+int main(void)
+{
+    int iCount;
+    
+    for (printf("Bitte geben Sie eine Zahl ein: "); iCount != 5;) 
+    {
+        scanf("%d", &iCount);
+    }
+}
