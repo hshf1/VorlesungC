b81c5425a96d4e7090b4db52d902c9cfb6f7b137
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-05-04 17:34:06 +0200

    Create bsplaengeZkette.c

---
+++ Codes aus Folien/Folien - Teil 3/bsplaengeZkette.c
@@ -0,0 +1,15 @@
+// Folie 3 - bsplaengeZkette.c
+ 
+#include <stdio.h>
+ 
+int main(void){
+    char s[] = "Mein Text";
+    int len = 0;
+ 
+    while (s[len] != 0 )
+    {
+        len++;
+    }
+    
+    printf("Anzahl Zeichen: %d\n", len);
+}
