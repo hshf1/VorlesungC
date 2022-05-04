4340e12785b355762810b310d668a18234f8d2b1
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-05-04 15:52:23 +0200

    Create ausgabeshortlong.c

---
+++ Codes aus Folien/Folien - Teil 3/ausgabeshortlong.c
@@ -0,0 +1,12 @@
+// Folie 3 - ausgabeshortlong.c
+ 
+#include <stdio.h>
+ 
+int main(void)  
+{
+    printf("Long Integer-Zahl: %ld\n", 100000);
+    /* Zu großer Wert für short */
+    printf("Short Integer-Zahl: %hd\n", 100000);
+    printf("Long Double Float-Zahl: %Lf\n",100.45);
+    return 0;
+}
