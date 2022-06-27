2ae02267f68671d6879ffe0505f8ad66b26c65e1
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-06-27 16:50:35 +0200

    Update and rename DasErsteCProgrammV2.c to erstescprog2.c

---
+++ Codes aus Folien/Folien - Teil 2/erstescprog2.c
@@ -0,0 +1,18 @@
+/* 
+
+hier steht ein mehrzeiliger Kommentar
+
+*/
+
+// hier steht ein einzeiliger Kommentar
+
+// Folie 2 – erstescprog2.c
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+    printf("Hello World\n");
+    system("pause"); //Windows -> Mac 
+    //system( "read -n 1 -s -p \"Pause\"; echo");
+}
