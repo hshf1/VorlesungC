5c9bc077e211e868f67c5015603e87d29b0d4791
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-06-27 16:25:05 +0200

    Update and rename StrukturEinesCProgramms.c to strukturcprog.c

---
+++ Codes aus Folien/Folien - Teil 2/strukturcprog.c
@@ -0,0 +1,24 @@
+// Folie 2 – strukturcprog.c
+
+#include <stdio.h>
+
+const double MWST = 0.19;
+int Preis, Anzahl;
+
+int function1 (int Param1, int  Param2)
+{
+   int Wert = 0;
+   Wert = Param1 * Param2*MWST;
+   anweisung2;
+
+   return Wert;
+}
+
+int main (void)
+{
+   int Erg = 0;
+   Preis = 2;
+   Anzahl = 4;
+
+   Erg = function1(Preis, Anzahl);
+}
