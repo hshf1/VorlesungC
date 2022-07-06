e522300c975cde015a366748f23af0197ef57fe2
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-06 20:59:13 +0200

    Create bspsprintf.c

---
+++ Codes aus Folien/Folien - Teil 4/bspsprintf.c
@@ -0,0 +1,12 @@
+// Folie 4 – bspsprintf.c
+#include <stdio.h>
+
+int main(void) {
+    int i = 32768;
+    char zk[6];
+    sprintf(zk, "%d", i);
+    printf("Eine Zahl:\n");
+    printf("\t%d\n", i);
+    printf("4. Ziffer von links:\n");
+    printf("\t%c", zk[3]);
+}
