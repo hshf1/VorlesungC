7931a217333c293726efd10dfc61019a318b4a2a
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-06 16:25:50 +0200

    Create funktionsprototypen.c

---
+++ Codes aus Folien/Folien - Teil 4/funktionsprototypen.c
@@ -0,0 +1,20 @@
+// Folie 4 – funktionsprototypen.c
+#include <stdio.h>
+
+int eingabe(void);
+void ausgabe(int);
+
+int main(void) {
+    int Zahl;
+    printf("Gebe eine Zahl ein: ");
+    Zahl = eingabe();
+    ausgabe(Zahl);
+}
+int eingabe (void) {
+    int Zahl;
+    scanf("%d",&Zahl);
+    return Zahl;
+}
+void ausgabe (int Wert) {
+    printf("%d",Wert);
+}
