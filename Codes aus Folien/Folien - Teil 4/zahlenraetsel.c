12c8dc18961108e0dd63e9a7febaba70081eb630
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-06 19:44:23 +0200

    Create zahlenraetsel.c

---
+++ Codes aus Folien/Folien - Teil 4/zahlenraetsel.c
@@ -0,0 +1,17 @@
+// Folie 4 – zahlenraetsel.c
+#include <stdio.h>
+
+int Zahl = 1;
+
+void ZahlenTest() {
+    int Zahl =2;
+    printf("%d", Zahl);
+}
+
+int main() {
+    printf("%d", Zahl);
+    int Zahl = 3;
+    printf("%d", Zahl);
+    ZahlenTest();
+    printf("%d", Zahl);
+}
