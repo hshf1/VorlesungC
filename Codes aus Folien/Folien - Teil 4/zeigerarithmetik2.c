2562e8e6a5500afb15a34705c8d2c3dedd04b4ba
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-06 20:41:43 +0200

    Create zeigerarithmetik2.c

---
+++ Codes aus Folien/Folien - Teil 4/zeigerarithmetik2.c
@@ -0,0 +1,13 @@
+// Folie 4 - zeigerarithmetik2.c
+#include <stdio.h>
+
+int main(void) {
+    int Feld[] = {1,2,3};
+    int *pointer = Feld;
+
+    printf("%d\n", *pointer);
+    printf("%d\n", *(pointer + 1));
+    printf("%d\n", *(pointer++));
+    printf("%d\n", *(++pointer));
+    printf("%d\n", *(pointer - 1));
+}
