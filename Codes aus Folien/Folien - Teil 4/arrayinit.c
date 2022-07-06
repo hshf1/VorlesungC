41159bb9e7ba971bde9cd9ac73be1cd2621faa3b
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-06 19:53:07 +0200

    Create arrayinit.c

---
+++ Codes aus Folien/Folien - Teil 4/arrayinit.c
@@ -0,0 +1,13 @@
+// Folie 4 – arrayinit.c
+#include <stdio.h>
+
+int main(void) {
+    /* Initialisieren aller Felder mit 0 */
+    int Feld[10] = {0};
+    float Reihe[3] = {0};
+    /* Zuweisung */
+    Reihe[0] = 1.5;
+    Reihe[1] = 2.5;
+    Reihe[2] = 3.5;
+    Reihe[3] = 4.5;
+}
