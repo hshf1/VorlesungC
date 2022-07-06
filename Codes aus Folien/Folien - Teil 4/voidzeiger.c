f80aaa935a99e242807bdef9d2545b54904d532f
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-06 20:35:10 +0200

    Create voidzeiger.c

---
+++ Codes aus Folien/Folien - Teil 4/voidzeiger.c
@@ -0,0 +1,13 @@
+// Folie 4 – voidzeiger.c
+#include <stdio.h>
+
+int main(void) 
+{
+    void *Zeiger;
+    int Wert;
+
+    Wert = 10;
+    Zeiger = &Wert;
+    
+printf("pointer: Adr. %p, Wert %d\n", Zeiger, *(int *)Zeiger);
+}
