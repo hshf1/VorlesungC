453cf455d69b1c8e1f682b2f88c7d9d4250a493a
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-06 20:05:06 +0200

    Create arrayfrage.c

---
+++ Codes aus Folien/Folien - Teil 4/arrayfrage.c
@@ -0,0 +1,11 @@
+// Folie 4 – arrayfrage.c
+#include <stdio.h>
+
+int main() {
+    int Feld[5] = {3,2,1}; //Array mit 5 Werten
+    int i; 
+    for (i=0; i<5;i++) {
+        Feld[i] = Feld[i+1];
+        printf("%d ", Feld[i]);
+    }
+}
