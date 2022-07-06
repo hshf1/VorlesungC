14c0e1aee6a5a0539038955784c260c79fed2e56
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-06 16:38:32 +0200

    Create funktionsprototypen4.c

---
+++ Codes aus Folien/Folien - Teil 4/funktionsprototypen4.c
@@ -0,0 +1,15 @@
+// Folie 4 – funktionsprototypen4.c
+double CalcProduct (int Wert1, float Wert2)
+{
+    double Ergebnis;
+    Ergebnis = Wert1 * Wert2;
+    return Ergebnis;
+}
+
+int main(void)
+{
+    const int Zahl1= 3;
+    const float Zahl2= 4.5;
+    double Uebergabe;
+    Uebergabe = CalcProduct(Zahl1, Zahl2);
+}
