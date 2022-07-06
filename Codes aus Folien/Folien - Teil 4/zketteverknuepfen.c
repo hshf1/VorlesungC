8d845837dc96cf112e7b4a8d80379b1a04a6010e
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-06 21:07:51 +0200

    Create zketteverknuepfen.c

---
+++ Codes aus Folien/Folien - Teil 4/zketteverknuepfen.c
@@ -0,0 +1,19 @@
+// Folie 4 – zketteverknuepfen.c
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char   Eisbaer[10] = {"Eis"};
+    size_t Laenge, NewLaenge;
+    int    Groesse;
+	
+    Laenge    = strlen(Eisbaer);
+    NewLaenge = strlen("baer");
+    Groesse    = sizeof(Eisbaer)/ sizeof(char);
+    if (Groesse >= (Laenge + NewLaenge + 1)) {
+        strcat(Eisbaer, "baer");
+        puts(Eisbaer);
+    } else {
+        puts("Passt nicht");    
+    } 
+}
