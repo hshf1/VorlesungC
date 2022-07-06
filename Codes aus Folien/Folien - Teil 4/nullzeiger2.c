d3adbab6fe06380733a666fb84d8210aacca60e9
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-06 20:22:41 +0200

    Create nullzeiger2.c

---
+++ Codes aus Folien/Folien - Teil 4/nullzeiger2.c
@@ -0,0 +1,13 @@
+// Folie 4 – nullzeiger2.c
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int *Zeiger = NULL;
+    if (!Zeiger) { 
+        printf("Keine gueltige Adresse\n");
+    }
+    else {
+        *Zeiger = 3;
+    }
+}
