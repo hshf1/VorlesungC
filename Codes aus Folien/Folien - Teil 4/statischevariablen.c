c62dcbdfa01a79efdcf0c01c253ca849faf9c882
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-06 19:49:36 +0200

    Create statischevariablen.c

---
+++ Codes aus Folien/Folien - Teil 4/statischevariablen.c
@@ -0,0 +1,19 @@
+// Folie 4 – statischevariablen.c
+#include <stdio.h>
+
+int berechne(int);
+
+int main(void) {
+    int Erg;
+    Erg = berechne(3);
+    Erg = berechne(4);
+}
+
+int berechne(int Wert) {
+    int Ergebnis;
+    static int Zahl = 0;
+    Ergebnis = Wert * 3;
+    Zahl++;
+    printf("Aufr.Nr.:%d\n",Zahl);
+    return Ergebnis;
+}
