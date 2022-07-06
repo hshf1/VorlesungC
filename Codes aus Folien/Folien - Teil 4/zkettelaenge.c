44718940727d4a05748dff0e61afa10762f3c84f
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-06 21:03:25 +0200

    Create zkettelaenge.c

---
+++ Codes aus Folien/Folien - Teil 4/zkettelaenge.c
@@ -0,0 +1,18 @@
+// Folie 4 – zkettelaenge.c
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+    char Text[101];
+    size_t Laenge = 0;
+    puts("Geben Sie max 100 Zeichen ein\n");
+    fgets(Text,101,stdin);
+
+    Laenge = strlen(Text);
+    puts("\n\nSie haben folgendes eingegeben:\n");
+    puts(Text);
+    printf("Es wurden %d Zeichen eingegeben\n\n", Laenge);
+
+    system("Pause");
+}
