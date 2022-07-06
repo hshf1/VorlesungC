f2f11d466400404e1887b5245d732f08fa85285e
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-06 20:49:20 +0200

    Create zeigeralsparam.c

---
+++ Codes aus Folien/Folien - Teil 4/zeigeralsparam.c
@@ -0,0 +1,14 @@
+// Folie 4 – zeigeralsparam.c
+#include <stdio.h>
+
+void malZwei(float *Zahl) {
+    *Zahl = (*Zahl) * 2; 
+}
+
+int main(void) {
+    float Wert;
+    Wert = 2.5;
+    printf("%f\n", Wert); 
+    malZwei(&Wert);
+    printf("%f\n", Wert); 
+}
