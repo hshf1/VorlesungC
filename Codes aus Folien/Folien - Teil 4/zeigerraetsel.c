9db2eef6f438f65702dee8793422ee8886b9c535
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-06 20:15:13 +0200

    Create zeigerraetsel.c

---
+++ Codes aus Folien/Folien - Teil 4/zeigerraetsel.c
@@ -0,0 +1,12 @@
+// Folie 4 – zeigerraetsel.c
+#include <stdio.h>
+int main(void){
+    int *zeigerInt;    //Zeiger auf Integervariable
+    int *zeigerInt2;   //Zeiger auf Integervariable
+    short Summe = 2;
+    zeigerInt = &Summe;
+    zeigerInt2 = zeigerInt;
+    (*zeigerInt2)+=2;
+    (*zeigerInt)--;
+    printf("Summe: %d\n", Summe);
+}
