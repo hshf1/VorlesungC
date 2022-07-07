450e8372929dd659d41ae275df05a92f75e3722a
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-07 12:43:23 +0200

    Create bspstruktur3.c

---
+++ Codes aus Folien/Folien - Teil 4/bspstruktur3.c
@@ -0,0 +1,16 @@
+// Folie 4 – bspstruktur3.c
+#include <stdio.h>
+
+int main(void) {
+    float Preis = 1.50;
+    typedef struct ARTIKEL {
+        int Nummer;
+        float *Preis;
+        char Name[10];
+    } artikel_t;
+
+    artikel_t Art1 = {11231, &Preis, "USB-Kabel"};
+
+    printf("Artikel.name: %s\n", Art1.Name);
+    printf("Artikel.preis: %6.2f\n", *Art1.Preis);
+}
