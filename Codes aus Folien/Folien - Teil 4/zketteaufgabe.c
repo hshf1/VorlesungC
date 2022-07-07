b3c6b092766ad1d0d3f12939aa42a08b08d2dade
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-07 11:59:58 +0200

    Create zketteaufgabe.c

---
+++ Codes aus Folien/Folien - Teil 4/zketteaufgabe.c
@@ -0,0 +1,10 @@
+// Folie 4 – zketteaufgabe.c
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char *Text []   = {"Auto", "Hund", "Haus"}; //Stringtabelle Typ 1
+    char *Text2 [] = {"Auto", "Hund", "Haus"}; //Stringtabelle Typ 2
+    
+    printf("%s", Text[1]+1);
+}
