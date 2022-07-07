f5fb7e6a5efb161588894cb2a61380a79d1c7472
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-07 12:49:52 +0200

    Create zeigerstruktur.c

---
+++ Codes aus Folien/Folien - Teil 4/zeigerstruktur.c
@@ -0,0 +1,15 @@
+// Folie 4 – zeigerstruktur.c
+
+int main(void) {
+    struct wert {
+        float *Betrag;
+        float *Rate;
+    } Firstwert;
+
+    float Kosten;
+    float Zinsen;
+
+    Firstwert.Betrag = &Kosten;
+    Firstwert.Rate = &Zinsen;
+    *Firstwert.Betrag = 100.99;
+}
