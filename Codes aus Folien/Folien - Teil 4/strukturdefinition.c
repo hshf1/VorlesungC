a50c76d2369560b69e834d9af52cf0bf9f9edf10
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-07 13:16:16 +0200

    Create strukturdefinition.c

---
+++ Codes aus Folien/Folien - Teil 4/strukturdefinition.c
@@ -0,0 +1,12 @@
+// Folie 4 – strukturdefinition.c
+
+int main(void) {
+    struct Artikel {
+        int artikelnummer;
+        char artikelBezeichnung[20];
+        double preis;
+        float istBestand;
+        float minBestand;
+    } art;
+    art.artikelnummer = 1;
+}
