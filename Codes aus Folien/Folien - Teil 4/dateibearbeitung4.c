7c0e9eac01331b0a418b2fb0ae92fe1e8416d82f
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-07 13:44:32 +0200

    Create dateibearbeitung4.c

---
+++ Codes aus Folien/Folien - Teil 4/dateibearbeitung4.c
@@ -0,0 +1,17 @@
+// Folie 4 – dateibearbeitung4.c
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE* fp = fopen("meine_binaerdatei.dat", "wb");
+  
+    if(fp == NULL) {
+        printf("Fehler: Datei konnte nicht geoeffnet werden.\n");
+        exit(-1);
+    }
+
+    int messwerte[4] = {1, 2, 3, 4};
+    fwrite(messwerte, sizeof(int), 4, fp);
+
+    fclose(fp);
+}
