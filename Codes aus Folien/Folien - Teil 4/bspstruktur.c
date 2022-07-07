4f3aee335119bcf493869b1ed218c06cf1d3764f
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-07 12:28:24 +0200

    Create bspstruktur.c

---
+++ Codes aus Folien/Folien - Teil 4/bspstruktur.c
@@ -0,0 +1,16 @@
+// Folie 4 – bspstruktur.c
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    struct ARTIKEL{
+        int Nummer;
+        float Preis;
+        char Name[10];
+    };
+    struct ARTIKEL Art1 = {1, 1.50, "USB-Kabel"};
+
+    printf("Artikel.Name: %s", Art1.Name);
+    printf("Artikel.Preis: %6.2f", Art1.Preis);
+    system("pause");
+}
