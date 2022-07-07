48452d12cb5a606bdf5e8b95df137bfc827d0bac
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-07 12:39:28 +0200

    Create strukturinit.c

---
+++ Codes aus Folien/Folien - Teil 4/strukturinit.c
@@ -0,0 +1,13 @@
+// Folie 4 – strukturinit.c
+
+typedef struct ARTIKEL {
+    int Artikelnummer;
+    char ArtikelBezeichnung[20];
+    double Preis;
+    float IstBestand;
+    double MinBestand;
+} artikel_t;
+
+int main(void) {
+    artikel_t Art = { 1, "Apfel pro kg", 1.99, 5.0, 3.0};
+}
