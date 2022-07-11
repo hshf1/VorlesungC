02b8c69ee609446797fe7eaef291dc61793f0760
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-11 07:57:08 +0200

    Update bspFloat2.c

--- Codes aus Folien/Folien - Teil 3/bspFloat2.c
+++ Codes aus Folien/Folien - Teil 3/bspFloat2.c
@@ -4,17 +4,16 @@
  
 int main(void)
 {
-    float fZahlFl   = 123.456789;
+    float ZahlFl   = 123.456789;
  
-    printf("Float Format f        :#%f#\n",        fZahlFl);
-    printf("Float Format .4f      :#%.4f#\n",      fZahlFl);
-    printf("Float Format 8.4f     :#%8.4f#\n",     fZahlFl);
-    printf("Float Format 10.4f    :#%10.4f#\n",    fZahlFl);
-    printf("Float Format 10.4g    :#%10.4g#\n",    fZahlFl);
-    printf("Float Format +10.4f   :#%+10.4f#\n",   fZahlFl);
-    printf("Float Format -10.4f   :#%-10.4f#\n",   fZahlFl);
-    printf("Float Format +010.4f  :#%+010.4f#\n",  fZahlFl);
-    printf("Float Format -010.4f  :#%-010.4f#\n",  fZahlFl);
-    printf("Float Format -+010.4f :#%-+010.4f#\n", fZahlFl);
-    return  0;
+    printf("Float Format f        :#%f#\n",        ZahlFl);
+    printf("Float Format .4f      :#%.4f#\n",      ZahlFl);
+    printf("Float Format 8.4f     :#%8.4f#\n",     ZahlFl);
+    printf("Float Format 10.4f    :#%10.4f#\n",    ZahlFl);
+    printf("Float Format 10.4g    :#%10.4g#\n",    ZahlFl);
+    printf("Float Format +10.4f   :#%+10.4f#\n",   ZahlFl);
+    printf("Float Format -10.4f   :#%-10.4f#\n",   ZahlFl);
+    printf("Float Format +010.4f  :#%+010.4f#\n",  ZahlFl);
+    printf("Float Format -010.4f  :#%-010.4f#\n",  ZahlFl);
+    printf("Float Format -+010.4f :#%-+010.4f#\n", ZahlFl);
 }
