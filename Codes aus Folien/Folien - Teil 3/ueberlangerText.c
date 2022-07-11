057e0dc0483f1a38db9ad98e3c41d59f8f89554f
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-11 08:03:37 +0200

    Update ueberlangerText.c

--- Codes aus Folien/Folien - Teil 3/ueberlangerText.c
+++ Codes aus Folien/Folien - Teil 3/ueberlangerText.c
@@ -1,12 +1,10 @@
 // Folie 3 - ueberlangerText.c
  
 #include <stdio.h>
-#include <stdlib.h>
  
 int main(void)
 {
    printf("So kann man wirklich ganz endlos lange Textstrings" 
           "drucken, die so lang sind, dass"
           " Sie ueber eine Zeile hinausgehen\n\n");    
-   system("Pause");
 }
