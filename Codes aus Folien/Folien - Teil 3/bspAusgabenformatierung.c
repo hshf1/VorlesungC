fab99edc30d929aa79102bf5c1734f84f0f5a9ad
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-11 07:51:02 +0200

    Update bspAusgabenformatierung.c

--- Codes aus Folien/Folien - Teil 3/bspAusgabenformatierung.c
+++ Codes aus Folien/Folien - Teil 3/bspAusgabenformatierung.c
@@ -4,16 +4,16 @@
 
 int main(void)
 {
-    int iZahlINT = 0x8E;
-    short int sZahlNEG = -2;
-    float fZahlFLOAT = 3.467;
+    int ZahlINT = 0x8E;
+    short int ZahlNEG = -2;
+    float ZahlFLOAT = 3.467;
 
-    printf("Integer Format      d: #%d#\n", iZahlINT);
-    printf("Integer Format      c: #%c#\n", iZahlINT);
-    printf("Integer Format      x: #%x#\n", iZahlINT);
-    printf("Integer Format      X: #%X#\n", iZahlINT);
-    printf("Neg Integer Format  d: #%d#\n", sZahlNEG);
-    printf("Neg Integer Format  u: #%u#\n", sZahlNEG);
-    printf("Integer Format      X: #%X#\n", sZahlNEG);
-    printf("Float Format        f: #%f#\n", fZahlFLOAT);
+    printf("Integer Format      d: #%d#\n", ZahlINT);
+    printf("Integer Format      c: #%c#\n", ZahlINT);
+    printf("Integer Format      x: #%x#\n", ZahlINT);
+    printf("Integer Format      X: #%X#\n", ZahlINT);
+    printf("Neg Integer Format  d: #%d#\n", ZahlNEG);
+    printf("Neg Integer Format  u: #%u#\n", ZahlNEG);
+    printf("Integer Format      X: #%X#\n", ZahlNEG);
+    printf("Float Format        f: #%f#\n", ZahlFLOAT);
 }
