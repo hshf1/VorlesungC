bc30e86821146a69b85cddf2b5be5ed087359c1c
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-11 07:55:35 +0200

    Update bspFloat.c

--- Codes aus Folien/Folien - Teil 3/bspFloat.c
+++ Codes aus Folien/Folien - Teil 3/bspFloat.c
@@ -4,11 +4,10 @@
  
 int main(void)
 {
-    float fZahlFl   = 123.456789;
+    float ZahlFl   = 123.456789;
  
-    printf("Float Format f        :#%f#\n",        fZahlFl);
-    printf("Float Format .4f      :#%.4f#\n",      fZahlFl);
-    printf("Float Format 8.4f     :#%8.4f#\n",     fZahlFl);
-    printf("Float Format 10.4f    :#%10.4f#\n",    fZahlFl);
-    return  0;
+    printf("Float Format f        :#%f#\n",        ZahlFl);
+    printf("Float Format .4f      :#%.4f#\n",      ZahlFl);
+    printf("Float Format 8.4f     :#%8.4f#\n",     ZahlFl);
+    printf("Float Format 10.4f    :#%10.4f#\n",    ZahlFl);
 }
