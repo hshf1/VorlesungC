88168ec1949f48fdfb2ede94805f3a44ae108608
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-11 08:19:18 +0200

    Update bspanweisungsleiter.c

--- Codes aus Folien/Folien - Teil 3/bspanweisungsleiter.c
+++ Codes aus Folien/Folien - Teil 3/bspanweisungsleiter.c
@@ -4,23 +4,22 @@
 
 int main (void)
 {
-    int iZahlA;
-    int iZahlB;
-    int iCheck; /* Anzahl der eingelesenen Zeichen */
+    int ZahlA;
+    int ZahlB;
+    int Check; /* Anzahl der eingelesenen Zeichen */
 
     printf("Ganzzahl 1 soll durch Ganzzahl 2 geteilt werden.");
     printf("Geben Sie zwei Ganzzahlen ein. Format:Z1 Z2");
-    iCheck = scanf("%d %d",&iZahlA, &iZahlB);
-    printf("Check = %d\n", iCheck);
+    Check = scanf("%d %d",&ZahlA, &ZahlB);
+    printf("Check = %d\n", Check);
    
-    if (iCheck == 2) {
+    if (Check == 2) {
         /* Es sind zwei Zahlen gelesen worden. */
-        printf("%d / %d = %d\n", iZahlA, iZahlB, iZahlA / iZahlB);
-    } else if (iCheck == 1) { 
+        printf("%d / %d = %d\n", ZahlA, ZahlB, ZahlA / ZahlB);
+    } else if (Check == 1) { 
         /* Die erste Zahl wurde korrekt gelesen, die zweite nicht. */
         printf("Der zweite Wert ist nicht korrekt.\n");
     } else {
         printf("Keine korrekte Eingabe\n");
     }
-    return 0;
 }
