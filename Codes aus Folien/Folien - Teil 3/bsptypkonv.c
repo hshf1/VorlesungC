f991a3171044dd842fc3a3fb00c644a3cf5c93d8
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-07-11 07:48:24 +0200

    Update bsptypkonv.c

--- Codes aus Folien/Folien - Teil 3/bsptypkonv.c
+++ Codes aus Folien/Folien - Teil 3/bsptypkonv.c
@@ -4,31 +4,31 @@
 
 int main(void)
 {
-    int    iZahl   = 2;
-    double dResult = 0;
+    int    Zahl   = 2;
+    double Result = 0;
 
-    dResult = 1 / 2 * iZahl;
-    printf("Ergebnis: %.1f\n", dResult); /* Ergebnis: 0.0 */
+    Result = 1 / 2 * Zahl;
+    printf("Ergebnis: %.1f\n", Result); /* Ergebnis: 0.0 */
 
-    dResult = 1.0 / 2 * iZahl;
-    printf("Ergebnis: %.1f\n", dResult); /* Ergebnis: 1.0 */
+    Result = 1.0 / 2 * Zahl;
+    printf("Ergebnis: %.1f\n", Result); /* Ergebnis: 1.0 */
 
-    dResult = 1 / 2.0 * iZahl;
-    printf("Ergebnis: %.1f\n", dResult); /* Ergebnis: 1.0 */
+    Result = 1 / 2.0 * Zahl;
+    printf("Ergebnis: %.1f\n", Result); /* Ergebnis: 1.0 */
 
-    dResult = (double) 1 / 2 * iZahl;
-    printf("Ergebnis: %.1f\n", dResult); /* Ergebnis: 1.0 */
+    Result = (double) 1 / 2 * Zahl;
+    printf("Ergebnis: %.1f\n", Result); /* Ergebnis: 1.0 */
 
-    dResult = 1 /(double)2 * iZahl;
-    printf("Ergebnis: %.1f\n", dResult); /* Ergebnis: 1.0 */
+    Result = 1 /(double)2 * Zahl;
+    printf("Ergebnis: %.1f\n", Result); /* Ergebnis: 1.0 */
 
-    dResult = (double) ( 1/ 2 * iZahl);
-    printf("Ergebnis: %.1f\n", dResult); /* Ergebnis: 0.0 */
+    Result = (double) ( 1/ 2 * Zahl);
+    printf("Ergebnis: %.1f\n", Result); /* Ergebnis: 0.0 */
 
-    dResult = iZahl * 1/2;
-    printf("Ergebnis: %.1f\n", dResult); /* Ergebnis: 1.0 */
+    Result = Zahl * 1/2;
+    printf("Ergebnis: %.1f\n", Result); /* Ergebnis: 1.0 */
 
-    dResult = (double) 5 + 1/2;
-    printf("Ergebnis: %.1f\n", dResult); /* Ergebnis: 5.0 */
+    Result = (double) 5 + 1/2;
+    printf("Ergebnis: %.1f\n", Result); /* Ergebnis: 5.0 */
 }
 
