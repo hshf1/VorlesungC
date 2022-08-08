c5d6bea08074e6dae372e746c416fcc8bef3e6b3
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-08-08 20:39:17 +0200

    Update bspInt.c

--- Codes aus Folien/Folien - Teil 3/bspInt.c
+++ Codes aus Folien/Folien - Teil 3/bspInt.c
@@ -13,4 +13,6 @@ int main(void)
     printf("Integer Format -8d  :#%-8d#\n",   ZahlInt);
     printf("Integer Format 8.5d :#%8.5d#\n",  ZahlInt);
     printf("Integer Format 10.8d:#%10.8d#\n", ZahlInt);
+    
+    return 0;
 }
