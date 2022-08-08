cb25f87775b93902624e5965042e694d2cf905bf
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-08-08 22:05:34 +0200

    Update bspsprintf.c

--- Codes aus Folien/Folien - Teil 4/bspsprintf.c
+++ Codes aus Folien/Folien - Teil 4/bspsprintf.c
@@ -4,9 +4,12 @@
 int main(void) {
     int i = 32768;
     char zk[6];
+    
     sprintf(zk, "%d", i);
     printf("Eine Zahl:\n");
     printf("\t%d\n", i);
     printf("4. Ziffer von links:\n");
     printf("\t%c", zk[3]);
+    
+    return 0;
 }
