6cb1ba5541152223ebe7428654c420ed97827184
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-08-08 20:31:04 +0200

    Update bitweiseop.c

--- Codes aus Folien/Folien - Teil 3/bitweiseop.c
+++ Codes aus Folien/Folien - Teil 3/bitweiseop.c
@@ -3,10 +3,11 @@
 int main(void) {
     char Zahl  = 0b10100010; // Binäre Eingabe der Zahl 0xA2
     char Maske = 0b00001111; // Binäre Eingabe der Zahl 0xF
-
     char Result;    // Ergebnisvariable
     char Schiebung; // Ergebnis nach Shift
 
     Result = Zahl & Maske;
     Schiebung = Result <<1;
+    
+    return 0;
 }
