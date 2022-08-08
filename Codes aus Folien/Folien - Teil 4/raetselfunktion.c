9e1a4cfe21736ec4e853662de4b2f9fd3c8a3d65
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-08-08 21:24:00 +0200

    Update raetselfunktion.c

--- Codes aus Folien/Folien - Teil 4/raetselfunktion.c
+++ Codes aus Folien/Folien - Teil 4/raetselfunktion.c
@@ -2,18 +2,20 @@
 #include <stdio.h>
 
 int Quadrat(int Wert1) {   
-	return Wert1 * Wert1; 
+    return Wert1 * Wert1; 
 }	
 
 //Berechne (Wert1+Wert2)^2
 int SummeZumQuadrat(int Wert1, int Wert2) {
-	int Res = Quadrat(Wert1 + Wert2);
-	return Wert2; 
+    int Res = Quadrat(Wert1 + Wert2);
+    return Wert2; 
 }
 
 int main() {
-	int Zahl1 = 1; //lokale Variable
-	int Zahl2 = 2; 
+    int Zahl1 = 1; //lokale Variable
+    int Zahl2 = 2; 
 
-	printf("Zahl: %d",SummeZumQuadrat(Zahl1, Zahl2)); 
+    printf("Zahl: %d",SummeZumQuadrat(Zahl1, Zahl2));
+	
+    return 0;
 }
