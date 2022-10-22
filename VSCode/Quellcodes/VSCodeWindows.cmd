dbd48eac55f16675bbb63c6e1a90815c08f54415
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-10-23 00:16:21 +0200

    %% fehlten bei Variable uninstall

--- VSCode/Quellcodes/VSCodeWindows.cmd
+++ VSCode/Quellcodes/VSCodeWindows.cmd
@@ -13,7 +13,7 @@ set workspace= %APPDATA%\Code\User\C_Uebung.code-workspace
 set vscerweiterung= "C:\Program Files\Microsoft VS Code\bin\code"
 
 :: Wenn deinstallationsvariable auf true, dann deinstallation ausführen
-if uninstall==1 (
+if %uninstall%=="true" (
 
 :: Beginn Logdatei
 (
