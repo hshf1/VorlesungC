7f683d752b520bb5b6c1ac3bcf0994d6da4821f8
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-10-23 00:24:55 +0200

    Update VSCodeWindow1s.cmd

--- VSCode/Quellcodes/VSCodeWindow1s.cmd
+++ VSCode/Quellcodes/VSCodeWindow1s.cmd
@@ -13,7 +13,7 @@ set workspace= %APPDATA%\Code\User\C_Uebung.code-workspace
 set vscerweiterung= "C:\Program Files\Microsoft VS Code\bin\code"
 
 :: Wenn deinstallationsvariable auf true, dann deinstallation ausführen
-if %option%==1 (
+if %option% (
 :: Beginn Logdatei
 (
 echo ---------------------------------------------------------------------------------------------------------------------------------------------------------
