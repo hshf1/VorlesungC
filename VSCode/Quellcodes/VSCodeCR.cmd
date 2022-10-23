69401038d9d8e8434bcaa234a1590c4e03c4d62a
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-10-23 16:01:03 +0200

    V1.04

--- VSCode/Quellcodes/VSCodeCR.cmd
+++ VSCode/Quellcodes/VSCodeCR.cmd
@@ -1,67 +1,73 @@
 @echo off
 set mydate=%date%
 set mytime=%time%
+set logfile="%USERPROFILE%\Desktop\logVSC.txt"
+set settingsjson="%APPDATA%\Code\User\settings.json"
+set launchjson="%APPDATA%\Code\User\launch.json"
+set tasksjson="%APPDATA%\Code\User\tasks.json"
+set vscerweiterung="C:\Program Files\Microsoft VS Code\bin\code"
 
 (
 echo ---------------------------------------------------------------------------------------------------------------------------------------------------------
 echo ---------------------------------------------------------------------------------------------------------------------------------------------------------
-echo Logfile zur Installation am %mydate% um %mytime%.
+echo Logfile zur Installation am %mydate% um%mytime%.
 echo. 
-echo Hochschule Hannover 01.05.2022 V1.03 VSCode installation für den Computerraum der HsH.
+echo Hochschule Hannover 20.06.2022 V1.04 VSCode Installation für den Computerraum der HsH.
 echo.
-echo Die aktuelle Version gibt es unter https://github.com/hshf1/VorlesungC/blob/main/VSCode/
+echo Die aktuelle Version gibt es hier:
+echo https://github.com/hshf1/VorlesungC/blob/main/VSCode/01_Installationsanleitung.md
 echo.
-echo Bei Problemen könnt ihr hier sehen, was falsch gelaufen ist und unter dem Link
+echo Bei Problemen könnt ihr hier nach Lösungen schauen:
 echo https://github.com/hshf1/VorlesungC/blob/main/VSCode/03_Fehlerbehebung.md
-echo findet ihr mögliche Lösungen. Bei anhaltenden oder neuen Problemen diese Datei per Mail an euren Dozenten schicken.
+echo Bei anhaltenden oder neuen Problemen diese Datei per Mail an euren Dozenten schicken.
 echo.
-echo Wenn alles funktioniert, kann diese Datei gelöscht werden.
+echo Fehler sind an "Fehler :" zu erkennen. Sind keine Fehler vorhanden, dann kann diese Datei gelöscht werden.
 echo ---------------------------------------------------------------------------------------------------------------------------------------------------------
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 
 echo Betriebssystem wird ermittelt...
 FOR /F "usebackq tokens=3,4,5" %%i IN (`REG query "hklm\software\microsoft\windows NT\CurrentVersion" /v ProductName`) DO (
 echo Meldung: Ausführendes System: %%i %%j %%k
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 
 echo Umgebungsvariable wird gesetzt.
 setx Path "%USERPROFILE%\AppData\Local\Microsoft\WindowsApps;C:\Program Files (x86)\Dev-Cpp\MinGW64\bin"
-echo %Path% >> %USERPROFILE%\AppData\Local\Temp\pathaktuell.txt
-findstr Dev-Cpp\MinGW64\bin %USERPROFILE%\AppData\Local\Temp\pathaktuell.txt
+echo %Path% >> "%temp%\pathaktuell.txt"
+findstr Dev-Cpp\MinGW64\bin "%temp%\pathaktuell.txt"
 IF %errorlevel% == 0 (
 (echo Meldung: Umgebungsvariable wurde erfolgreich gesetzt.
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 ) ELSE (
 (echo Fehler: Umgebungsvariable konnte nicht gesetzt werden!
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 )
-del "%USERPROFILE%\AppData\Local\Temp\pathaktuell.txt"
+del "%temp%\pathaktuell.txt"
 
 echo Alte Einstellungen werden gesucht und ersetzt/erstellt...
-if NOT EXIST %APPDATA%\Code\User (
-mkdir %APPDATA%\Code\User\
+if NOT EXIST "%APPDATA%\Code\User" (
+mkdir "%APPDATA%\Code\User\"
 )
-if NOT EXIST %APPDATA%\Code\User\settings.json (
+if NOT EXIST %settingsjson% (
 (echo Meldung: Alte settings.json wurde nicht gefunden.
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 ) ELSE (
 (echo Meldung: Alte settings.json wurde gefunden.
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
-del "%APPDATA%\Code\User\settings.json"
-if NOT EXIST %APPDATA%\Code\User\settings.json (
+echo >CON) >> %logfile%
+del %settingsjson%
+if NOT EXIST %settingsjson% (
 (echo Meldung: Alte settings.json wurde erfolgreich entfernt.
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 ) ELSE (
 (echo Fehler: Alte settings.json konnte nicht erfolgreich entfernt werden!
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 )
 )
 (echo {
@@ -79,6 +85,8 @@ echo "editor.insertSpaces": true,                    // Ersezt ein Tab durch Lee
 echo "editor.tabSize": 4,                            // Setzt die Zahl der durch einen Tab zu ersetzenden Leerzeichen
 echo "editor.renderWhitespace": "none",              // Zeigt keine Leerzeichen ein 
 echo //"editor.renderWhitespace": "selection",       // Nur im markierten Bereich Leerzeichen anzeigen
+echo "extensions.autoUpdate": false,                 // Deaktivieren von Auto-Updates der Extensions
+echo "C_Cpp.debugShortcut": false,                   // Deaktivieren der instabilen neue Funktion von C/C++ Erweiterung
 echo "code-runner.runInTerminal": true,              // Um Eingaben in seinem Programm tätigen zu können z.B. für scanf
 echo "code-runner.preserveFocus": false,             // damit springt man automatisch ins Terminal bei Abarbeitung
 echo "code-runner.executorMap": {
@@ -120,34 +128,34 @@ echo       "preLaunchTask": "C/C++: gcc.exe Aktive Datei kompilieren"
 echo   }]
 echo }
 echo }
-echo >CON) > %APPDATA%\Code\User\settings.json
-if EXIST %APPDATA%\Code\User\settings.json (
+echo >CON) > %settingsjson%
+if EXIST %settingsjson% (
 (echo Meldung: Neue settings.json wurde erfolgreich erstellt.
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 ) ELSE (
 (echo Fehler: Neue settings.json konnte nicht erfolgreich erstellt werden!
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 )
 
-if NOT EXIST %APPDATA%\Code\User\launch.json (
+if NOT EXIST %launchjson% (
 (echo Meldung: Alte launch.json wurde nicht gefunden.
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 ) ELSE (
 (echo Meldung: Alte launch.json wurde gefunden.
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
-del "%APPDATA%\Code\User\launch.json"
-if NOT EXIST %APPDATA%\Code\User\launch.json (
+echo >CON) >> %logfile%
+del %launchjson%
+if NOT EXIST %launchjson% (
 (echo Meldung: Alte launch.json wurde erfolgreich entfernt.
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 ) ELSE (
 (echo Fehler: Alte launch.json konnte nicht erfolgreich entfernt werden!
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 )
 )
 (echo {
@@ -179,34 +187,34 @@ echo        "preLaunchTask": "C/C++: gcc.exe Aktive Datei kompilieren"
 echo    }
 echo ]
 echo } 
-echo >CON ) > %APPDATA%\Code\User\launch.json
-if EXIST %APPDATA%\Code\User\launch.json (
+echo >CON ) > %launchjson%
+if EXIST %launchjson% (
 (echo Meldung: Neue launch.json wurde erfolgreich erstellt.
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 ) ELSE (
 (echo Fehler: Neue launch.json konnte nicht erfolgreich erstellt werden!
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 )
 
-if NOT EXIST %APPDATA%\Code\User\tasks.json (
+if NOT EXIST %tasksjson% (
 (echo Meldung: Alte tasks.json wurde nicht gefunden.
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 ) ELSE (
 (echo Meldung: Alte tasks.json wurde gefunden.
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
-del "%APPDATA%\Code\User\tasks.json"
-if NOT EXIST %APPDATA%\Code\User\tasks.json (
+echo >CON) >> %logfile%
+del %tasksjson%
+if NOT EXIST %tasksjson% (
 (echo Meldung: Alte tasks.json wurde erfolgreich entfernt.
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 ) ELSE (
 (echo Fehler: Alte tasks.json konnte nicht erfolgreich entfernt werden!
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 )
 )
 (echo {
@@ -236,55 +244,66 @@ echo		"detail": "Compiler: C:\\Program Files (x86)\\Dev-Cpp\\MinGW64\\bin\\gcc.e
 echo	}
 echo ]
 echo }
-echo >CON ) > %APPDATA%\Code\User\tasks.json
-if EXIST %APPDATA%\Code\User\tasks.json (
+echo >CON ) > %tasksjson%
+if EXIST %tasksjson% (
 (echo Meldung: Neue tasks.json wurde erfolgreich erstellt.
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 ) ELSE (
-(echo Fehler: Neue tasks.json konnte nicht erfolgreich erstellt werden!
+(echo Fehler : Neue tasks.json konnte nicht erfolgreich erstellt werden!
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 )
 
 echo VSCode Extensions werden installiert...
-call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension formulahendry.code-runner
+call %vscerweiterung% --install-extension formulahendry.code-runner
 if %errorlevel% == 0 (
 (echo Meldung: Code-Runner Extension wurde/ist installiert.
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 ) ELSE (
-(echo Fehler: Bei der Installation von der Code-Runner Extension ist ein Fehler aufgetreten!
+(echo Fehler : Bei der Installation von der Code-Runner Extension ist ein Fehler aufgetreten!
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 )
-call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension ms-vscode.cpptools
+call %vscerweiterung% --install-extension ms-vscode.cpptools@1.9.8
 if %errorlevel% == 0 (
 (echo Meldung: C/C++ Extension wurde/ist installiert.
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 ) ELSE (
 (echo Fehler: Bei der Installation von der C/C++ Extension ist ein Fehler aufgetreten!
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 )
-call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension ms-vsliveshare.vsliveshare-pack
+call %vscerweiterung% --install-extension ms-vsliveshare.vsliveshare
 if %errorlevel% == 0 (
 (echo Meldung: Live Share Extension wurde/ist installiert.
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 ) ELSE (
 (echo Fehler: Bei der Installation von der Live Share Extension ist ein Fehler aufgetreten!
 echo.
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
+)
+call %vscerweiterung% --install-extension ms-vsliveshare.vsliveshare-audio
+if %errorlevel% == 0 (
+(echo Meldung: Live Share Audio Extension wurde/ist installiert.
+echo.
+echo >CON) >> %logfile%
+) ELSE (
+(echo Fehler: Bei der Installation von der Live Share Audio Extension ist ein Fehler aufgetreten!
+echo.
+echo >CON) >> %logfile%
 )
 
 (
 echo Installation beendet!
 echo ---------------------------------------------------------------------------------------------------------------------------------------------------------
-echo >CON) >> %USERPROFILE%/Desktop/logVSC.txt
+echo >CON) >> %logfile%
 
 echo msgbox"Installation beendet.",vbInformation , "Installation beendet!"> %temp%\msg.vbs 
-%Temp%\msg.vbs 
+%temp%\msg.vbs 
 erase %temp%\msg.vbs
-cmd /c start %USERPROFILE%/Desktop/logVSC.txt
+start "" %logfile%
+EXIT /B
