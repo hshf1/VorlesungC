ac6d198ef2fa537251bdad0963b1013837362c23
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-10-26 06:01:24 +0200

    Add files via upload

--- VSCode/Quellcodes/Computerraum/VSCode.cmd
+++ VSCode/Quellcodes/Computerraum/VSCode.cmd
@@ -84,65 +84,7 @@ echo.
 echo >CON) >> %logfile%
 )
 )
-(echo {
-echo    // Allgemeine Nutzereinstellungen
-echo "liveshare.anonymousGuestApproval": "accept",   // Live Share eingeladene Anonyme Nutzer automatisch akzeptieren
-echo "liveshare.guestApprovalRequired": false,       // Live Share um eingeladene Nutzer automatisch zu akzeptieren auf false einstellen
-echo "extensions.ignoreRecommendations": true,       // Keine Empfehlungen mehr Anzeigen
-echo "files.encoding": "cp437",                      // Zur richtigen Darstellung von Umlauten
-echo //"files.autoGuessEncoding": true,              // Zurzeit, deaktiviert, da noch instabil! Automatische Anpassung der Encodierung, falls möglich
-echo "editor.unicodeHighlight.nonBasicASCII": false, // Nicht Basic ASCII Zeichen nicht hervorheben
-echo "files.autoSave": "onFocusChange",              // Dateien werden bei Änderungen des Fokus automatisch gespeichert
-echo "code-runner.saveFileBeforeRun": true,          // speichert aktuelle Datei bevor sie mit CodeRunner ausgeführt wird
-echo "editor.bracketPairColorization.enabled": true, // Um Klammern und andere farbig darzustellen
-echo "editor.insertSpaces": true,                    // Ersezt ein Tab durch Leerzeichen
-echo "editor.tabSize": 4,                            // Setzt die Zahl der durch einen Tab zu ersetzenden Leerzeichen
-echo "editor.renderWhitespace": "none",              // Zeigt keine Leerzeichen ein 
-echo //"editor.renderWhitespace": "selection",       // Nur im markierten Bereich Leerzeichen anzeigen
-echo "extensions.autoUpdate": false,                 // Deaktivieren von Auto-Updates der Extensions
-echo "C_Cpp.debugShortcut": false,                   // Deaktivieren der nicht getesteten neuen Funktion von C/C++ Erweiterung
-echo "code-runner.runInTerminal": true,              // Um Eingaben in seinem Programm tätigen zu können z.B. für scanf
-echo "code-runner.preserveFocus": false,             // damit springt man automatisch ins Terminal bei Abarbeitung
-echo "code-runner.executorMap": {
-echo   "c": "clear && cd $dir && gcc $fileName -o $fileNameWithoutExt && $dir$fileNameWithoutExt"
-echo },
-echo "editor.tokenColorCustomizations": {
-echo   "textMateRules": [
-echo       {
-echo           "scope": "keyword",
-echo           "settings": {
-echo               "fontStyle": "bold"
-echo           }
-echo       },
-echo   ]
-echo },
-echo    // Einstellungen für den Debugger
-echo "launch": {
-echo   "version": "0.2.0",
-echo   "configurations": [{
-echo       "type": "cppdbg",
-echo       "request": "launch",
-echo       "name": "debuggen",
-echo       "program": "${fileDirname}\\${fileBasenameNoExtension}.exe",
-echo       "args": [],
-echo       "stopAtEntry": false,
-echo       "cwd": "${workspaceFolder}",
-echo       "environment": [],
-echo       "externalConsole": false,
-echo       "MIMode": "gdb",
-echo       "miDebuggerPath": "C:\\Program Files (x86)\\Dev-Cpp\\MinGW64\\bin\\gdb.exe",
-echo       "setupCommands": [
-echo           {
-echo               "description": "Automatische Strukturierung und Einrückung für \"gdb\" aktivieren",
-echo               "text": "-enable-pretty-printing",
-echo               "ignoreFailures": true
-echo           }
-echo       ],
-echo       "preLaunchTask": "C/C++: gcc.exe Aktive Datei kompilieren"
-echo   }]
-echo }
-echo }
-echo >CON) > %settingsjson%
+
 if EXIST %settingsjson% (
 (echo Meldung: Neue settings.json wurde erfolgreich erstellt.
 echo.
@@ -172,36 +114,7 @@ echo.
 echo >CON) >> %logfile%
 )
 )
-(echo {
-echo // Use IntelliSense to learn about possible attributes.
-echo // Hover to view descriptions of existing attributes.
-echo // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
-echo "version": "0.2.0",
-echo "configurations": [
-echo   {
-echo        "name": "gcc.exe - Aktive Datei erstellen und debuggen",
-echo        "type": "cppdbg",
-echo        "request": "launch",
-echo        "program": "${fileDirname}\\${fileBasenameNoExtension}.exe",
-echo        "args": [],
-echo        "stopAtEntry": false,
-echo        "cwd": "${workspaceFolder}",
-echo        "environment": [],
-echo        "externalConsole": false,//"externalTerminal",
-echo        "MIMode": "gdb",
-echo        "miDebuggerPath": "C:\\Program Files (x86)\\Dev-Cpp\\MinGW64\\bin\\gdb.exe",
-echo        "setupCommands": [
-echo            {
-echo                "description": "Automatische Strukturierung und Einrückung für \"gdb\" aktivieren",
-echo                "text": "-enable-pretty-printing",
-echo                "ignoreFailures": true
-echo            }
-echo        ],
-echo        "preLaunchTask": "C/C++: gcc.exe Aktive Datei kompilieren"
-echo    }
-echo ]
-echo } 
-echo >CON ) > %launchjson%
+
 if EXIST %launchjson% (
 (echo Meldung: Neue launch.json wurde erfolgreich erstellt.
 echo.
@@ -231,34 +144,8 @@ echo.
 echo >CON) >> %logfile%
 )
 )
-(echo {
-echo "version": "2.0.0",
-echo "tasks": [
-echo 	{
-echo		"type": "cppbuild",
-echo		"label": "C/C++: gcc.exe Aktive Datei kompilieren",
-echo		"command": "C:\\Program Files (x86)\\Dev-Cpp\\MinGW64\\bin\\gcc.exe",
-echo		"args": [
-echo			"-g",
-echo			"${file}",
-echo			"-o",
-echo			"${fileDirname}\\${fileBasenameNoExtension}.exe"
-echo		],
-echo		"options": {
-echo			"cwd": "${workspaceFolder}"
-echo		},
-echo		"problemMatcher": [
-echo			"$gcc"
-echo		],
-echo		"group": {
-echo			"kind": "build",
-echo			"isDefault": true
-echo		},
-echo		"detail": "Compiler: C:\\Program Files (x86)\\Dev-Cpp\\MinGW64\\bin\\gcc.exe"
-echo	}
-echo ]
-echo }
-echo >CON ) > %tasksjson%
+
+
 if EXIST %tasksjson% (
 (echo Meldung: Neue tasks.json wurde erfolgreich erstellt.
 echo.
