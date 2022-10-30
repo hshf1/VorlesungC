b3bc8caad35a277550c754d7141d2fa35f140100
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-10-30 20:33:06 +0100

    Kommentare und Pfade angepasst

--- VSCode/Quellcodes/Computerraum/VSCode.cmd
+++ VSCode/Quellcodes/Computerraum/VSCode.cmd
@@ -14,8 +14,6 @@
 :: dont show used commands in terminal
 @echo off
 
-:::::::::: begin install ::::::::::
-
 :: add to user environment variables
 for /f "usebackq tokens=2,*" %%A in (`reg query HKCU\Environment /v PATH`) do set my_user_path=%%B
 setx PATH "C:\Program Files (x86)\Dev-Cpp\MinGW64\bin;%my_user_path%"
@@ -24,13 +22,13 @@ setx VSCODE_EXTENSIONS U:\.vscode\extensions
 :: change direction for global settings folder in environment variable for more information look https://github.com/microsoft/vscode/blob/a5f84617e22e6e32afc18a808828f1e233361244/src/paths.js
 setx VSCODE_APPDATA U:\.vscode
 :: create/overwrite settings.json and create direction if not exist
-curl --create-dirs -o U:/.vscode/Code/User/settings.json https://raw.githubusercontent.com/hshf1/VorlesungC/main/VSCode/Quellcodes/Computerraum/settings.json
+curl --create-dirs -o U:\.vscode\Code\User\settings.json https://raw.githubusercontent.com/hshf1/VorlesungC/main/VSCode/Quellcodes/Computerraum/settings.json
 :: create/overwrite launch.json
-curl -o U:/.vscode/Code/User/launch.json https://raw.githubusercontent.com/hshf1/VorlesungC/main/VSCode/Quellcodes/Computerraum/launch.json
+curl -o U:\.vscode\Code\User\launch.json https://raw.githubusercontent.com/hshf1/VorlesungC/main/VSCode/Quellcodes/Computerraum/launch.json
 :: create/overwrite tasks.json
-curl -o U:/.vscode/Code/User/tasks.json https://github.com/hshf1/VorlesungC/blob/main/VSCode/Quellcodes/Computerraum/tasks.json
+curl -o U:\.vscode\Code\User\tasks.json https://github.com/hshf1/VorlesungC/blob/main/VSCode/Quellcodes/Computerraum/tasks.json
 :: create/overwrite testprog.c and create direction if not exist - usage is to test debugger and coderunner
-curl --create-dirs -o U:/C_Uebung/testprog.c https://raw.githubusercontent.com/hshf1/VorlesungC/main/VSCode/Quellcodes/Computerraum/testprog.c
+curl --create-dirs -o U:\C_Uebung\testprog.c https://raw.githubusercontent.com/hshf1/VorlesungC/main/VSCode/Quellcodes/Computerraum/testprog.c
 :: set installpath for extensions
 code --extensions-dir U:\.vscode\extensions
 :: install vscode extension code-runner
@@ -44,8 +42,6 @@ call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension ms-vslive
 :: create batchfile to start vsc with the folge C_Uebung and ensure the environment paths
 echo start "" "C:\Program Files\Microsoft VS Code\Code.exe" -r U:\C_Uebung > "U:\Systemordner\Desktop\C_Uebung.cmd"
 echo EXIT /B >> "U:\Systemordner\Desktop\C_Uebung.cmd"
-
-:::::::::: end install ::::::::::
 
 :: popup install finished
 echo msgbox"Installation beendet.", vbInformation, "Installation beendet!" > %temp%\msg.vbs 
