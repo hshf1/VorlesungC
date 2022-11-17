fb33867c5c60e370ba98847537e6f3f9356b6387
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-11-17 22:37:36 +0100

    Finale Anpassung (muss getestet werden)

--- VSCode/Quellcodes/Computerraum/VSCode.cmd
+++ VSCode/Quellcodes/Computerraum/VSCode.cmd
@@ -1,20 +1,19 @@
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-::                          VSCode Installation for Computerraum ::
-::                   created for Hochschule Hannover Vorlesung C ::
-::       created by Can Kocak | 19.04.2022 | Hochschule Hannover ::
-:: last modified by Can Kocak | 31.10.2022 | Hochschule Hannover ::
+::                          VSCode Installation für Computerraum ::
+::                               Hochschule Hannover Vorlesung C ::
+::                                zuletzt geändert am 17.11.2022 ::
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-:: to install the configuration for VSCode open terminal (no need for being administrator) and execute following code (copy as oneline without ::)
+:: zum installieren Terminal öffnen (keine Adminrechte nötig!) und folgende Zeile Code ausführen (ohne ::)
 ::
 :: curl -o %temp%\VSCode.cmd https://raw.githubusercontent.com/hshf1/VorlesungC/main/VSCode/Quellcodes/Computerraum/VSCode.cmd && %temp%\VSCode.cmd && del %temp%\VSCode.cmd && EXIT /B
-::
-:: description: curl to downloads file, %temp%\VSCode.cmd to start script, del to delete script after finished, EXIT /B to exit script and close terminal
 
-:: dont show used commands in terminal
+:: Auszuführende Befehle nicht nochmal im Terminal anzeigen
 @echo off
 
-:: add to user environment variables
+:::: Beginn Installation ::::
+
+:: Umgebungsvariable setzen um code zu nutzen und libraries zu finden
 for /f "usebackq tokens=2,*" %%A in (`reg query HKCU\Environment /v PATH`) do set my_user_path=%%B
 setx PATH "C:\Program Files (x86)\Dev-Cpp\MinGW64\bin;%my_user_path%"
 :: change direction for extensions in environment varbiable for more information look https://github.com/microsoft/vscode/blob/a5f84617e22e6e32afc18a808828f1e233361244/src/vs/platform/environment/node/environmentService.ts#L121
@@ -31,22 +30,24 @@ curl -o U:\.vscode\Code\User\tasks.json https://github.com/hshf1/VorlesungC/blob
 curl --create-dirs -o U:\C_Uebung\testprog.c https://raw.githubusercontent.com/hshf1/VorlesungC/main/VSCode/Quellcodes/Computerraum/testprog.c
 :: set installpath for extensions
 code --extensions-dir U:\.vscode\extensions
-:: install vscode extension code-runner
+:: VSCode Extension code-runner installieren
 call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension formulahendry.code-runner
-:: install vscode extension C/C++
+:: VSCode Extension C/C++ installieren
 call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension ms-vscode.cpptools
-:: install vscode extension Liveshare
+:: VSCode Extension Liveshare installieren
 call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension ms-vsliveshare.vsliveshare
-:: install vscode extension Liveshare-Audio
+:: VSCode Extension Liveshare-Audio installieren
 call "C:\Program Files\Microsoft VS Code\bin\code" --install-extension ms-vsliveshare.vsliveshare-audio
-:: create batchfile to start vsc with the folder C_Uebung
+:: Alternative zu Workspacefolder - Batchfile auf Desktop erstellen/überschreiben, C_Uebung.cmd kann überall verschoben und umbenannt werden
 echo start "" "C:\Program Files\Microsoft VS Code\Code.exe" -r U:\C_Uebung > "U:\Systemordner\Desktop\C_Uebung.cmd"
 echo EXIT /B >> "U:\Systemordner\Desktop\C_Uebung.cmd"
 
-:: popup install finished
+:::: Ende Installation ::::
+
+:: Popup Skript beendet
 echo msgbox"Installation beendet.", vbInformation, "Installation beendet!" > %temp%\msg.vbs 
 %temp%\msg.vbs 
 erase %temp%\msg.vbs
 
-:: exit script and close terminal
+:: exit skript und Terminal schließen
 EXIT /B
