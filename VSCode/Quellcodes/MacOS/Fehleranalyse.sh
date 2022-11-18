d1c69205f1ed7a31d6baadd2fc0ef0b3d4b99d37
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-11-18 02:03:50 +0100

    Fehlercodes eingefügt

--- VSCode/Quellcodes/MacOS/Fehleranalyse.sh
+++ VSCode/Quellcodes/MacOS/Fehleranalyse.sh
@@ -20,49 +20,49 @@ softwareinfo=$(sw_vers)
 # Internetverbindung prüfen
 ping -c 1 www.google.com
 if (($? == 0)); then
-    pinginfo="Meldung: Es konnte eine Verbindung zum Internet erkannt werden!"
+    pinginfo="Internetverbindung: Es konnte eine Verbindung zum Internet erkannt werden."
 else
-    pinginfo="Meldung: Es konnte keine Verbindung zum Internet erkannt werden!"
+    pinginfo="Internetverbindung: Es konnte keine Verbindung zum Internet erkannt werden! (Fehlercode: 0002)"
 fi
 
 # Prüfen, ob VSCode installiert ist
 if [ -e "/Applications/Visual Studio Code.app" ]; then
-    vscinfo2="Meldung: VSCode ist im richtigen Ordner bereits installiert."
+    vscinfo2="VSCode: VSCode ist installiert."
 else
-    vscinfo2="Meldung: VSCode konnte nicht gefunden werden!"
+    vscinfo2="VSCode: VSCode ist nicht installiert oder konnte nicht gefunden werden! (Fehlercode: 0003)"
 fi
 
 # Prüfen, ob Compiler installiert ist
 clang --version
 if (($? == 0)); then
     gccversion=$(clang --version)
-    gccinfo="Meldung: Compiler bereits installiert.\nCompiler-Version: $gccversion"
+    gccinfo="Compiler: Compiler ist installiert.\nCompiler-Version: $gccversion"
 else
-    gccinfo="Meldung: Compiler konnte nicht erkannt werden!"
+    gccinfo="Compiler: Compiler ist nicht installiert oder konnte nicht erkannt werden! (Fehlercode: 0004)"
 fi
 
 # Prüfen, ob settings.json vorhanden ist (Inhalt wird nicht überprüft!)
 file=~/Library/Application\ Support/Code/User/settings.json
 if [ -e "$file" ]; then
-    settingsinfo="Meldung: settings.json wurde gefunden."
+    settingsinfo="settings.json: $file wurde gefunden."
 else
-    settingsinfo="Meldung: settings.json konnte nicht gefunden werden!"
+    settingsinfo="settings.json: $file konnte nicht gefunden werden! (Fehlercode: 0005)"
 fi
 
 # Prüfen, ob launch.json vorhanden ist (Inhalt wird nicht überprüft!)
 file=~/Library/Application\ Support/Code/User/launch.json
 if [ -e "$file" ]; then
-    launchinfo="Meldung: launch.json wurde gefunden."
+    launchinfo="launch.json: $file wurde gefunden."
 else
-    launchinfo="Meldung: launch.json konnte nicht gefunden werden!"
+    launchinfo="launch.json: $file konnte nicht gefunden werden! (Fehlercode: 0005)"
 fi
 
 # Prüfen, ob tasks.json vorhanden ist (Inhalt wird nicht überprüft!)
 file=~/Library/Application\ Support/Code/User/tasks.json
 if [ -e "$file" ]; then
-    tasksinfo="Meldung: tasks.json wurde gefunden."
+    tasksinfo="tasks.json: $file wurde gefunden."
 else
-    tasksinfo="Meldung: tasks.json konnte nicht gefunden werden!"
+    tasksinfo="tasks.json: $file konnte nicht gefunden werden! (Fehlercode: 0005)"
 fi
 
 # Umgebungsvariable setzen und aktualisieren um code zu nutzen
@@ -75,34 +75,34 @@ EOF
 source ~/.bash_profile
 
 # Liste installierter Extensions in Variable setzen
-installedextensions=$(code --list-extensions)
+installedextensions=$(code --list-extensions --show-versions)
 
 # Prüfen, ob VSCode Extension code-runner installiert ist
 if echo "$installedextensions" | grep 'code-runner' > /dev/null 2>&1; then
-    coderunnerinfo="Meldung: Die Extension Code-Runner wurde erfolgreich installiert."
+    coderunnerinfo="Code-Runner: Die Extension Code-Runner ist installiert."
 else
-    coderunnerinfo="Fehler: Bei der Installation der Extension Code-Runner trat ein Fehler auf."
+    coderunnerinfo="Code-Runner: Die Extension Code-Runner konnte nicht gefunden werden. (Fehlercode: 0006)"
 fi
 
 # Prüfen, ob VSCode Extension cpptools installiert ist
 if echo "$installedextensions" | grep 'cpptools' > /dev/null 2>&1; then
-    cppinfo="Meldung: Die Extension C/C++ wurde erfolgreich installiert."
+    cppinfo="C/C++: Die Extension C/C++ ist installiert."
 else
-    cppinfo="Fehler: Bei der Installation der Extension C/C++ trat ein Fehler auf."
+    cppinfo="C/C++: Die Extension C/C++ konnte nicht gefunden werden. Fehlercode: 0006)"
 fi
 
 # Prüfen, ob VSCode Extension LiveShare installiert ist
 if echo "$installedextensions" | grep 'liveshare' > /dev/null 2>&1; then
-    liveshareinfo="Meldung: Die Extension Live Share wurde erfolgreich installiert."
+    liveshareinfo="LiveShare: Die Extension LiveShare ist installiert."
 else
-    liveshareinfo="Fehler: Bei der Installation der Extension Live Share trat ein Fehler auf."
+    liveshareinfo="LiveShare: Die Extension LiveShare konnte nicht gefunden werden. (Fehlercode: 0006)"
 fi
 
 # Prüfen, ob VSCode Extension lldb installiert ist
 if echo "$installedextensions" | grep 'lldb' > /dev/null 2>&1; then
-    lldbinfo="Meldung: Die Extension lldb Compiler wurde erfolgreich installiert."
+    lldbinfo="lldb: Die Extension lldb ist installiert."
 else
-    lldbinfo="Fehler: Bei der Installation der Extension lldb Compiler trat ein Fehler auf."
+    lldbinfo="lldb: Die Extension lldb konnte nicht gefunden werden. (Fehlercode: 0006)"
 fi
 
 # LogFile schreiben und Variablen einsetzen
@@ -118,6 +118,9 @@ VSCode Installation: https://github.com/hshf1/VorlesungC/blob/main/VSCode/01_Ins
 Einige Fehler können hier entdeckt werden und sind am Ende der Zeile mit einem Fehlercode bezeichnet.
 Unter Fehlerbebehung: https://github.com/hshf1/VorlesungC/blob/main/VSCode/03_Fehlerbehebung.md
 kann nach den Fehlercodes gesucht werden, um zu sehen, wie diese behoben werden können.
+
+Datei- und Ordnernamen dürfen keine Umlaute oder Leerzeichen enthalten!
+Zum Debuggen muss in VSCode ein Ordner geöffnet sein und keine einzelne Datei!
 
 Bei anhaltenden oder neuen Problemen diese Datei per Mail an euren Dozenten schicken.
 -------------------------------------------------------------------------------------------
@@ -144,6 +147,9 @@ $cppinfo
 $liveshareinfo
 
 $lldbinfo
+
+Versionen installierter VSCode Extensions:
+$installedextensions
 
 Fehleranalyse beendet!
 -------------------------------------------------------------------------------------------
