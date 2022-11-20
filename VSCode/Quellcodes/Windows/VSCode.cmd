c73aa69082c840af98327e05b2b9e31401b83da3
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-11-20 17:36:56 +0100

    call choco angepasst

--- VSCode/Quellcodes/Windows/VSCode.cmd
+++ VSCode/Quellcodes/Windows/VSCode.cmd
@@ -35,15 +35,10 @@ if /i "%~1"=="uninstall" (GOTO UNINSTALL)
 :::: Beginn Installation ::::
 
 :: installiere choco, wenn nicht vorhanden
-choco -v
-if %errorlevel% == 0 (
-    echo. >nul
-) ELSE (
-    call %systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('https://community.chocolatey.org/install.ps1','%temp%/installChoco.ps1'))"
-    rd /s /q C:\ProgramData\chocolatey
-    call %systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%temp%/installChoco.ps1' %*"
-    del "%temp%\installChoco.ps1"
-)
+call %systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('https://community.chocolatey.org/install.ps1','%temp%/installChoco.ps1'))"
+rd /s /q C:\ProgramData\chocolatey
+call %systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%temp%/installChoco.ps1' %*"
+del "%temp%\installChoco.ps1"
 
 :: Umgebungsvariable setzen um code zu nutzen und libraries zu finden
 for /f "usebackq tokens=2,*" %%A in (`reg query HKCU\Environment /v PATH`) do set my_user_path=%%B
