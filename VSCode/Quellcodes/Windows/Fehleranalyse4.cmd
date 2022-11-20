59bfa90c919d50cd2bff555202348305411d4ec6
cakohsh <100713757+cakohsh@users.noreply.github.com>
2022-11-20 03:24:50 +0100

    Update and rename Fehleranalyse3.cmd to Fehleranalyse4.cmd

--- VSCode/Quellcodes/Windows/Fehleranalyse4.cmd
+++ VSCode/Quellcodes/Windows/Fehleranalyse4.cmd
@@ -30,23 +30,23 @@
 
 :: Info zum Betriebssystem
 FOR /F "usebackq tokens=3,4,5" %%i IN (`REG query "hklm\software\microsoft\windows NT\CurrentVersion" /v ProductName`) DO (
-    echo Ausführendes Betriebssystem: & echo %%i %%j %%k & echo. >> "%USERPROFILE%\Desktop\logVSC.txt"
+( echo Ausführendes Betriebssystem: & echo %%i %%j %%k & echo. ) >> "%USERPROFILE%\Desktop\logVSC.txt"
 )
 
 :: Internetverbindung prüfen
 ping -n 1 google.de>NUL
 if %errorlevel% == 0 (
-    echo Internetverbindung: Es konnte eine Verbindung zum Internet erkannt werden. & echo. >> "%USERPROFILE%\Desktop\logVSC.txt"
+    ( echo Internetverbindung: Es konnte eine Verbindung zum Internet erkannt werden. & echo.) >> "%USERPROFILE%\Desktop\logVSC.txt"
 ) ELSE (
-    echo Internetverbindung: Es konnte keine Verbindung zum Internet erkannt werden! ^(Fehlercode: 0002^) & echo. >> "%USERPROFILE%\Desktop\logVSC.txt"
+    ( echo Internetverbindung: Es konnte keine Verbindung zum Internet erkannt werden! ^(Fehlercode: 0002^) & echo. ) >> "%USERPROFILE%\Desktop\logVSC.txt"
 )
 
 :: Prüfen, ob choco installiert ist
 choco -v>NUL
 if %errorlevel% == 0 (
-    echo Choco: Choco ist installiert und hat folgende Version: & choco -v & echo. >> "%USERPROFILE%\Desktop\logVSC.txt"
+    ( echo Choco: Choco ist installiert und hat folgende Version: & choco -v & echo. ) >> "%USERPROFILE%\Desktop\logVSC.txt"
 ) ELSE (
-    echo Choco: Choco konnte nicht gefunden werden! & echo. >> "%USERPROFILE%\Desktop\logVSC.txt"
+    ( echo Choco: Choco konnte nicht gefunden werden! & echo. ) >> "%USERPROFILE%\Desktop\logVSC.txt"
 )
 
     :: Prüfen, ob VSCode installiert ist
