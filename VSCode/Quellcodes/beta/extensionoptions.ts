a7e820a3fbb006f023105947b20a53f1648c2770
cakohsh <100713757+cakohsh@users.noreply.github.com>
2023-01-06 09:38:09 +0100

    Update and rename extensionoptions.js to extensionoptions.ts

---
+++ VSCode/Quellcodes/beta/extensionoptions.ts
@@ -0,0 +1,12 @@
+gueltig_bis = '07.01.2023';
+
+moodle_ckurs1prof = 'Prof. Will';
+moodle_ckurs1 = 'www.google.de';
+
+moodle_ckurs1prof = "Prof. Imiela";
+moodle_ckurs2 = "www.moodle.hs-hannover.de";
+
+moodle_ckurs1prof = "Prof. Homann";
+moodle_ckurs2 = "www.hs-hannover.de";
+
+Aufgabe1_gueltig_bis = "07.01.2023";
