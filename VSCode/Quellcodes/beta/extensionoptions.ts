7f80e64f43502deb35a92ed6a4762ffa867c47e3
cakohsh <100713757+cakohsh@users.noreply.github.com>
2023-01-06 07:08:36 +0100

    test

---
+++ VSCode/Quellcodes/beta/extensionoptions.ts
@@ -0,0 +1,12 @@
+export const gueltig_bis = "07.01.2023"
+
+export const moodle_ckurs1prof = "Prof. Will"
+export const moodle_ckurs1 = "www.google.de"
+
+export const moodle_ckurs1prof = "Prof. Imiela"
+export const moodle_ckurs2 = "www.moodle.hs-hannover.de"
+
+export const moodle_ckurs1prof = "Prof. Homann"
+export const moodle_ckurs2 = "www.hs-hannover.de"
+
+export const Aufgabe1_gueltig_bis = "07.01.2023"
