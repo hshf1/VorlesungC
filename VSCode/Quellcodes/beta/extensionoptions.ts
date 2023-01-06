7e46e6acdf2016050d6f2eb413aa44a004f5cad4
cakohsh <100713757+cakohsh@users.noreply.github.com>
2023-01-06 09:40:12 +0100

    Update extensionoptions.ts

--- VSCode/Quellcodes/beta/extensionoptions.ts
+++ VSCode/Quellcodes/beta/extensionoptions.ts
@@ -1,7 +1,7 @@
-gueltig_bis = '07.01.2023';
+gueltig_bis = "07.01.2023";
 
-moodle_ckurs1prof = 'Prof. Will';
-moodle_ckurs1 = 'www.google.de';
+moodle_ckurs1prof = "Prof. Will";
+moodle_ckurs1 = "www.google.de";
 
 moodle_ckurs1prof = "Prof. Imiela";
 moodle_ckurs2 = "www.moodle.hs-hannover.de";
