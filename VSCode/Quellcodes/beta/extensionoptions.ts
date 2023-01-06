3467d07d4cf187c7e05e1f5867733ed588577ab2
cakohsh <100713757+cakohsh@users.noreply.github.com>
2023-01-06 09:22:44 +0100

    Update extensionoptions.ts

--- VSCode/Quellcodes/beta/extensionoptions.ts
+++ VSCode/Quellcodes/beta/extensionoptions.ts
@@ -1,7 +1,7 @@
-export const gueltig_bis = "07.01.2023";
+export const gueltig_bis = '07.01.2023';
 
-export const moodle_ckurs1prof = "Prof. Will";
-export const moodle_ckurs1 = "www.google.de";
+export const moodle_ckurs1prof = 'Prof. Will';
+export const moodle_ckurs1 = 'www.google.de';
 
 export const moodle_ckurs1prof = "Prof. Imiela";
 export const moodle_ckurs2 = "www.moodle.hs-hannover.de";
