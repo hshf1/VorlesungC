172d7c998888019d0eb8dfb81dded976045ef91b
cakohsh <100713757+cakohsh@users.noreply.github.com>
2023-01-07 04:31:41 +0100

    Update extensionoptions.ts

--- VSCode/Quellcodes/beta/extensionoptions.ts
+++ VSCode/Quellcodes/beta/extensionoptions.ts
@@ -1,11 +1,11 @@
 ckurs_prof1 = "Prof. Will";
 ckurs_link1 = "https://moodle.hs-hannover.de/course/view.php?id=21067";
-gueltig_bis1 = "08.01.2023";
+gueltig_bis1 = "";
 
 ckurs_prof2 = "Prof. Imiela";
 ckurs_link2 = "moodle.hs-hannover.de";
+gueltig_bis2 = "07.01.2023";
 
 ckurs_prof3 = "Prof. Homann";
 ckurs_link3 = "hs-hannover.de";
-
-quiz_active = "nein";
+gueltig_bis3 = "06.01.2023";
