d462a188acc278ffee2facfdf54406506835c60b
cakohsh <100713757+cakohsh@users.noreply.github.com>
2023-01-07 05:27:24 +0100

    Create links.ts

---
+++ VSCode/Quellcodes/AddOn4VSC/links.ts
@@ -0,0 +1,11 @@
+link_name1 = "Moodle-Kurs - Prof. Will";
+link1 = "https://moodle.hs-hannover.de/course/view.php?id=21067";
+gueltig_bis1 = "";
+
+link_name2 = "Moodle-Kurs - Prof. Imiela";
+link2 = "https://moodle.hs-hannover.de";
+gueltig_bis2 = "06.01.2023";
+
+link_name3 = "Moodle- Kurs - Prof. Homann";
+link3 = "https://hs-hannover.de";
+gueltig_bis3 = "06.01.2023";
