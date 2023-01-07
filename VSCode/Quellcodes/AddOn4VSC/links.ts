e2924ee97c11cd8988c3119618fd213a67514666
cakohsh <100713757+cakohsh@users.noreply.github.com>
2023-01-07 07:06:34 +0100

    Update links.ts

--- VSCode/Quellcodes/AddOn4VSC/links.ts
+++ VSCode/Quellcodes/AddOn4VSC/links.ts
@@ -6,6 +6,6 @@ link_name2 = "Moodle-Kurs - Prof. Imiela";
 link2 = "https://moodle.hs-hannover.de";
 gueltig_bis2 = "06.01.2023";
 
-link_name3 = "Moodle- Kurs - Prof. Homann";
+link_name3 = "Moodle-Kurs - Prof. Homann";
 link3 = "https://hs-hannover.de";
 gueltig_bis3 = "06.01.2023";
