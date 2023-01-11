83f5507e18d247fe48c3b149403f57bdc6e80457
cakohsh <100713757+cakohsh@users.noreply.github.com>
2023-01-11 23:40:14 +0100

    Update and rename quiz.json to quiz.ts

---
+++ VSCode/Quellcodes/AddOn4VSC/quiz.ts
@@ -0,0 +1,19 @@
+question: 'C ist eine objektorientierte Programmiersprache';
+answers: ['Wahr', 'Falsch', 'Lieber am Handy sein'];
+correctAnswer: 'Falsch';
+
+question: 'Fluss- und Struktogramme drücken unterschiedliche Dinge aus.';
+answers: ['Wahr', 'Falsch'];
+correctAnswer: 'Falsch';
+
+question: 'Womit beginnt die kopfgesteuerte Schleife?';
+answers: ['Bedingung', 'Anweisung'];
+correctAnswer: 'Bedingung';
+
+question: 'Wie groß ist ein Byte in Bit?';
+answers: ['0', '1', '4', '8', '10', 'keiner dieser Antworten'];
+correctAnswer: '8';
+   
+question: 'Ein Algorithmus benötigt endlich viele Arbeitsschritte.';
+answers: ['Wahr', 'Falsch'];
+correctAnswer: 'Wahr';
