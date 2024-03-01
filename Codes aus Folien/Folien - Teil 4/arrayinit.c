742ed9b0695bf8a19a1f0580c08d408139858236
JCWHSH <81768351+JCWHSH@users.noreply.github.com>
2024-03-01 11:28:10 +0100

    Variablennamen korrigiert

--- Codes aus Folien/Folien - Teil 4/arrayinit.c
+++ Codes aus Folien/Folien - Teil 4/arrayinit.c
@@ -1,15 +1,15 @@
 // Folie 4 – arrayinit.c
 #include <stdio.h>
 
-int main(void) {
-    /* Initialisieren aller Felder mit 0 */
-    int Feld[10] = {0};
-    float Reihe[3] = {0};
-    /* Zuweisung */
-    Reihe[0] = 1.5;
-    Reihe[1] = 2.5;
-    Reihe[2] = 3.5;
-    Reihe[3] = 4.5;
-    
-    return 0;
+int main(void) {
+    /* Initialisieren aller Felder mit 0 */
+    int feld[10] = {0};
+    float reihe[3] = {0};
+    /* Zuweisung */
+    reihe[0] = 1.5;
+    reihe[1] = 2.5;
+    reihe[2] = 3.5;
+    reihe[3] = 4.5;
+    return 0;
 }
+
