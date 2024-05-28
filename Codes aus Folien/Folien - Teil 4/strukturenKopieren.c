f3ca0a58dce68985bee0c3693480fbb6b130de9c
LCNjo <121254360+LCNjo@users.noreply.github.com>
2024-05-28 10:26:00 +0200

    Update strukturenKopieren.c

--- Codes aus Folien/Folien - Teil 4/strukturenKopieren.c
+++ Codes aus Folien/Folien - Teil 4/strukturenKopieren.c
@@ -1,1 +1,31 @@
+// Folie 4 – StrukturenKopieren.c
+#include <stdio.h>
+typedef struct ARTIKEL {
+	int Nummer;
+
+	float Preis;
+	char Name[11];
+} artikel_t;
+
+
+int main(void) {
+    artikel_t ArtFeld[5] = {{1, 1.10, "Leer"}};
+    int i;
+  
+    for (i = 0; i< 5; i++) {
+        printf("%d \t%10.2f\t%s\n", ArtFeld[i].Nummer,
+        ArtFeld[i].Preis, ArtFeld[i].Name);
+    }
+
+
+    for (i = 1; i< 5; i++) ArtFeld[i]=ArtFeld[0];
+
+    for (i = 0; i< 5; i++) {
+        printf("%d \t%10.2f\t%s\n", ArtFeld[i].Nummer,
+        ArtFeld[i].Preis, ArtFeld[i].Name);
+    }
+
+
+    return 0; 
+}
 
