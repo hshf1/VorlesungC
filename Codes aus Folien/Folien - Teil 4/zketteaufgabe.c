ef58b7b7e61d4f3e6fa1212088ffa9891b1c1085
LCNjo <121254360+LCNjo@users.noreply.github.com>
2024-12-06 09:56:32 +0100

    Update zketteaufgabe.c

--- Codes aus Folien/Folien - Teil 4/zketteaufgabe.c
+++ Codes aus Folien/Folien - Teil 4/zketteaufgabe.c
@@ -4,9 +4,10 @@
 
 int main(void) {
     char *Text []   = {"Auto", "Hund", "Haus"}; //Stringtabelle Typ 1
-    char *Text2 [] = {"Auto", "Hund", "Haus"}; //Stringtabelle Typ 2
+    char Text2 [3][5] = {"Auto", "Hund", "Haus"}; //Stringtabelle Typ 2
     
-    printf("%s", Text[1]+1);
+    printf("%s\n", Text[1]+1);
+    printf("%s\n", Text2[1]+1);
     
     return 0;
 }
