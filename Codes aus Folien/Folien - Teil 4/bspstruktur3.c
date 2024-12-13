4cc0026b0676d0d30368902e04f9521394bfc687
LCNjo <121254360+LCNjo@users.noreply.github.com>
2024-12-13 09:56:14 +0100

    Update bspstruktur3.c

--- Codes aus Folien/Folien - Teil 4/bspstruktur3.c
+++ Codes aus Folien/Folien - Teil 4/bspstruktur3.c
@@ -1,18 +1,20 @@
 // Folie 4 – bspstruktur3.c
 #include <stdio.h>
 
+
+typedef struct artikel_s {
+    int Nummer;
+    float *Preis;
+    char Name[10];
+} artikel_t;
+
+
 int main(void) {
     float Preis = 1.50;
-    typedef struct ARTIKEL {
-        int Nummer;
-        float *Preis;
-        char Name[10];
-    } artikel_t;
-
     artikel_t Art1 = {11231, &Preis, "USB-Kabel"};
 
-    printf("Artikel.name: %s\n", Art1.Name);
-    printf("Artikel.preis: %6.2f\n", *Art1.Preis);
+    printf("Artikelname: %s\n", Art1.Name);
+    printf("Artikelpreis: %6.2f\n", *Art1.Preis);
     
     return 0;
 }
