e91f43a62316edb4e72fc23393126b90bf57b075
LCNjo <121254360+LCNjo@users.noreply.github.com>
2024-12-13 10:33:10 +0100

    Update verkettetelisten.c

--- Codes aus Folien/Folien - Teil 4/verkettetelisten.c
+++ Codes aus Folien/Folien - Teil 4/verkettetelisten.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct PERSON_S person_t;
-struct PERSON_S {
+typedef struct person_s person_t;
+struct person_s {
     char *Name;
     char *Adresse;
     int PLZ;
@@ -14,7 +14,7 @@ int main (){
     person_t *StartListe=NULL;          // Startpunkt der Liste definieren
     person_t *Pers=NULL;                // Zeiger auf Aktuelle Person definieren
 
-    Pers=calloc(1,sizeof(person_t));    // Speicher für Person anfordern
+    Pers=(person_s*)calloc(1,sizeof(person_t));    // Speicher für Person anfordern
     if (Pers) {                         // Wenn Speicher erhalten dann...
         StartListe=Pers;                // Startpunkt auf erste Person legen
         Pers->Name=(char*)malloc(31);   // Speicher für Name anfordern
@@ -22,7 +22,7 @@ int main (){
         Pers->Nachbar=NULL;             // Weiteres Kettenelement auf NULL setzten
     }
   
-    Pers=calloc(1,sizeof(person_t));    // Speicher für nächste Person anfordern
+    Pers=(person_s*)calloc(1,sizeof(person_t));    // Speicher für nächste Person anfordern
     if (Pers) {                         // Wenn Speicher erhalten, dann...
         StartListe->Nachbar=Pers;       // Nachbar von Startelement mit akt: Person verkn.
         Pers->Nachbar=NULL;             // Weiteres Kettenelement auf NULL setzten
