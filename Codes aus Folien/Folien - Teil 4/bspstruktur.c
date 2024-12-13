d62830c2554104706c804de5d8a7cc2e6999a151
LCNjo <121254360+LCNjo@users.noreply.github.com>
2024-12-13 09:51:43 +0100

    Update bspstruktur.c

--- Codes aus Folien/Folien - Teil 4/bspstruktur.c
+++ Codes aus Folien/Folien - Teil 4/bspstruktur.c
@@ -2,16 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-    struct ARTIKEL{
-        int Nummer;
-        float Preis;
-        char Name[10];
-    };
-    struct ARTIKEL Art1 = {1, 1.50, "USB-Kabel"};
+struct artikel_s{
+    int Nummer;
+    float Preis;
+    char Name[10];
+};
 
-    printf("Artikel.Name: %s", Art1.Name);
-    printf("Artikel.Preis: %6.2f", Art1.Preis);
+int main(void) {
+
+    struct artikel_s Art1 = {1, 1.50, "USB-Kabel"};
+
+    printf("Artikel-Name: %s", Art1.Name);
+    printf("Artikel-Preis: %6.2f", Art1.Preis);
     system("pause");
     
     return 0;
